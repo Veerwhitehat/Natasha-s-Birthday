5444443cd49ab3e053013e503ba220048b4fc3f2
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2022-05-09 10:46:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload() {
+    
+}
+function setup() {
+    
+    canvas=createCanvas(640,480);
+    canvas.position(450,30);
+    video= createCapture(VIDEO);
+    video.hide();
+    tint_color="";
+    
+}
+function draw() {
+    
+    image(video,0,0,640,480);
+    tint(tint_color);
+
+}
+function take_snapshot() {
+    
+    save("Birthday.png");
+
+}
