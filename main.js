e1da01b8c8fcd324719b9137e8ff91de9de7e889
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2022-05-11 16:20:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,24 @@ function setup() {
     canvas.position(450,30);
     video= createCapture(VIDEO);
     video.hide();
-    tint_color="";
+  
     
 }
 function draw() {
     
     image(video,0,0,640,480);
-    tint(tint_color);
+   fill ("yellow");
+   stroke("black");
+   circle (30,30,50);
+   circle (610,30,50);
+   circle (30,450,50);
+   circle (610,450,50);
+   fill ("aqua");
+   stroke("black");
+   rect (90,40,450,20);
+   rect (90,425,450,20);
+   rect (70,65,20,350);
+   rect (540,65,20,350);
 
 }
 function take_snapshot() {
